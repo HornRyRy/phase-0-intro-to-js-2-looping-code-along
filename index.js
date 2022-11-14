66864b21d88b6571ccd160a31054bb15ca3b0ed9
Ryan Horn <rj.horn@gmail.com>
2022-11-14 11:02:07 -0700

    commit tree 434588cefb88cba9a930b1a6374e2e322dbf86ff

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solutions in this file
+
+for (let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`)
+    debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+
+//const peeps = ["Charlie", "Samip", "Ali"];
+
+
+function writeCards(arrayOfPeeps, event){
+
+    let i = 0;
+    let giftMessages = [];
+    while (i < arrayOfPeeps.length){
+        
+        
+        giftMessages[i] = `Thank you, ${arrayOfPeeps[i]}, for the wonderful ${event} gift!`; 
+        
+        i++;
+
+    }
+    return giftMessages;
+    
+}
+
+function countDown(number){
+    let i = 0;
+    while (number >= i){
+        console.log(number);
+        number = number -1; 
+    }
+
+}
